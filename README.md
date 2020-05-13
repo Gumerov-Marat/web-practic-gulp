92e3759c98e48e6e0993b519e3bcae463eaefc86
Marat Gumerov <gumerovm74@gmail.com>
2020-05-13 15:19:15 +0300

    start

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # web-practic-gulp
+
+//  npm i gulp-autoprefixer --save-dev
+//  npm install --save-dev gulp-cssbeautify
+// npm i gulp-strip-css-comments --save-dev  (удаление коментариев)
+// npm i gulp rename --save-dev  (переименовывает файлы )
+// npm i gulp-sass --save-dev
+// npm i gulp-cssnano --save-dev (сжимает ксс)
+// npm i gulp-rigger --save-dev (склеивает js файлы)
+// npm i gulp-uglify --save-dev (сжатие js файлов)
+// npm i gulp-plumber --save-dev (ошибки в js коде не ломает gulp)
+// npm i gulp-imagemin --save-dev
+// npm i del --save-dev (очистка папки где скомпилирован сайт)
+// npm i panini --save-dev (для html)
+// npm i browser-sync --save-dev
---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const {src, dest} = require("gulp");
+const gulp = require("gulp");
+const autoptrfixer = require("gulp-autoprefixer");
+const cssbeautify = require("gulp-cssbeautify");
+const removeComments = require('gulp-strip-css-comments');
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const cssnano = require("gulp-cssnano");
+const rigger = require("gulp-rigger");
+const uglify = require("gulp-uglify");
+const plumber = require("gulp-plumber");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
+const panini = require("panini");
+const browsersync = require("browser-sync").create();
+
+
+var path = {
+    build: {
+      html: 'dist/',
+      js: 'dist/assets/js/',
+      css: 'dist/assets/css/',
+      images: 'dist/assets/img/'
+    },
+    src:{
+      html: 'src/*.html',
+        js: 'src/assets/js/*.js',
+        css: 'src/assets/sass/style.scss',
+        images: 'src/assets/img/**/*.{jpg,png,svg,gif,ico}'
+    },
+    watch: {
+      html: 'src/**/*.html',
+      js: 'src/assets/js/**/*.js',
+      css: 'src/assets/sass/**/style.scss',
+      images: 'src/assets/img/**/*.{jpg,png,svg,gif,ico}'
+    },
+    clean: "./dist"
+}
+
